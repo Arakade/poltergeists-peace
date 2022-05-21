5d2694085550970852b15a151fb8a9566dee7af1
Rupert Key <Rupert.Key@GMail.com>
2022-05-21 15:37:06 +0100

    NPC nNavigation starting to work.

---
+++ Assets/_Game/03Code/npc/NPCMovement.cs
@@ -0,0 +1,49 @@
+﻿
+#nullable enable
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ghostly.npc {
+	public sealed class NPCMovement : MonoBehaviour {
+#region serialized
+		
+		[SerializeField]
+		private NavMeshAgent agent = null!;
+
+#endregion serialized
+#region Unity callbacks
+
+		public void Awake() {
+			wayPoints = FindObjectsOfType<WayPoint>();
+			
+			agent.updateRotation = false;
+			agent.updateUpAxis = false;
+		}
+
+		public void Start() {
+			if (null == wayPoints || 0 >= wayPoints.Length) {
+				Debug.LogWarning($"No waypoints for {this}");
+				return;
+			}
+				
+			// var destWP = wayPoints[Random.Range(0, wayPoints.Length)];
+			var destWP = wayPoints[0];
+			var pos = destWP.transform.position;
+			Debug.Log($"{this} going to {destWP} = {pos}");
+			agent.SetDestination(pos);
+		}
+
+#endregion Unity callbacks
+#region public
+
+#endregion public
+#region internal
+
+#endregion internal
+#region private
+		
+		private WayPoint[]? wayPoints;
+
+#endregion private
+	}
+}
---
+++ Assets/_Game/03Code/pathfinding/CreateNavMeshModifierOnChildren.cs
@@ -0,0 +1,51 @@
+﻿#nullable enable
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ghostly.pathfinding {
+	public sealed class CreateNavMeshModifierOnChildren : MonoBehaviour {
+#region serialized
+
+		[SerializeField] private int area = 1;
+
+#endregion serialized
+#region Unity callbacks
+
+		[ContextMenu("Add obstacles to child colliders")]
+		private void addObstaclesToChildColliders() {
+			var children = GetComponentsInChildren<Collider2D>();
+			foreach (var c in children) {
+				// If it already has a NMM, skip
+				if (null != (c.GetComponent<NavMeshModifier>())) {
+					continue;
+				}
+
+				var obstacle = c.gameObject.AddComponent<NavMeshModifier>();
+				obstacle.area = area;
+				obstacle.overrideArea = true;
+			}
+
+			// NavMeshAssetManager.instance.StartBakingSurfaces(targets);
+		}
+
+
+		[ContextMenu("Remove obstacles from all children")]
+		private void removeObstaclesFromChildren() {
+			var children = GetComponentsInChildren<NavMeshModifier>();
+			foreach (var obstacle in children) {
+				DestroyImmediate(obstacle);
+			}
+		}
+
+#endregion Unity callbacks
+#region public
+
+#endregion public
+#region internal
+
+#endregion internal
+#region private
+
+#endregion private
+	}
+}
